4027c5060db1303f8aa6f55ddc8911d71852b908
CaptainCodewastaken <87475832+CaptainCodewastaken@users.noreply.github.com>
2024-06-12 22:58:09 +0530

    modified:   pages/about.js

--- pages/about.js
+++ pages/about.js
@@ -15,10 +15,10 @@ const About = () => {
             At our core, we believe in the power of storytelling to foster empathy, understanding, and connection. Every story shared here is a testament to the resilience, creativity, and diversity of our community.
           </p>
           <p className="text-lg leading-relaxed mt-4">
-            Our platform is open to everyone, regardless of background or experience. Whether you're a seasoned writer, an aspiring storyteller, or simply someone with a tale to tell, we invite you to join us on this journey of discovery and expression.
+            Our platform is open to everyone, regardless of background or experience. Whether you&apos;re a seasoned writer, an aspiring storyteller, or simply someone with a tale to tell, we invite you to join us on this journey of discovery and expression.
           </p>
           <p className="text-lg leading-relaxed mt-4">
-            Thank you for being a part of our story. Together, let's celebrate the voices, experiences, and narratives that make Kolkata truly unique.
+            Thank you for being a part of our story. Together, let&apos;s celebrate the voices, experiences, and narratives that make Kolkata truly unique.
           </p>
         </div>
       </div>
